fca6a44bdfe88bc48830740278f362c4d4507abe
Rodolfo Bogado <rodolfoosvaldobogado@gmail.com>
2014-04-28 20:13:46 -0300

    small merge from master: fix in peek z in DX11/Render.cpp

--- Source/Core/VideoBackends/DX11/Render.cpp
+++ Source/Core/VideoBackends/DX11/Render.cpp
@@ -397,7 +397,7 @@ u32 Renderer::AccessEFB(EFBAccessType type, u32 x, u32 y, u32 poke_data)
 								&RectToLock,
 								Renderer::GetTargetWidth(),
 								Renderer::GetTargetHeight(),
-								PixelShaderCache::GetDepthMatrixProgram(true),
+								PixelShaderCache::GetColorCopyProgram(true),
 								VertexShaderCache::GetSimpleVertexShader(),
 								VertexShaderCache::GetSimpleInputLayout());
 
@@ -738,10 +738,10 @@ bool Renderer::SaveScreenshot(const std::string &filename, const TargetRectangle
 	// D3DX11SaveTextureToFileA doesn't allow us to ignore the alpha channel, so we need to strip it out ourselves
 	D3D11_MAPPED_SUBRESOURCE map;
 	D3D::context->Map(s_screenshot_texture, 0, D3D11_MAP_READ_WRITE, 0, &map);
-	for (unsigned int y = 0; y < rc.GetHeight(); ++y)
+	for (s32 y = 0; y < rc.GetHeight(); ++y)
 	{
 		u8* ptr = (u8*)map.pData + y * map.RowPitch + 3;
-		for (unsigned int x = 0; x < rc.GetWidth(); ++x)
+		for (s32 x = 0; x < rc.GetWidth(); ++x)
 		{
 			*ptr = 0xFF;
 			ptr += 4;
