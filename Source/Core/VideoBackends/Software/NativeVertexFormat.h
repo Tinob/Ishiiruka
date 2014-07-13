81aee0f1a56213e88cc4d34f056b19c00ac74e71
Rodolfo Bogado <rodolfoosvaldobogado@gmail.com>
2014-07-13 20:00:09 -0300

    missed comments in header

--- Source/Core/VideoBackends/Software/NativeVertexFormat.h
+++ Source/Core/VideoBackends/Software/NativeVertexFormat.h
@@ -1,9 +1,9 @@
 // Copyright 2013 Dolphin Emulator Project
 // Licensed under GPLv2
 // Refer to the license.txt file included.
+// Modified for Ishiiruka by Tino
 
-#ifndef _NATIVEVERTEXFORMAT_H
-#define _NATIVEVERTEXFORMAT_H
+#pragma once
 
 #include "Vec3.h"
 #include "Common/ChunkFile.h"
@@ -86,5 +86,3 @@ struct OutputVertexData
 	}
 
 };
-
-#endif
