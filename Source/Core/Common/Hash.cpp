143b4cce30173b47d290e62464b53274f835b4fa
Rodolfo Bogado <rodolfoosvaldobogado@gmail.com>
2015-01-21 19:59:04 -0300

    replace murmurhash3 with XXhash

--- Source/Core/Common/Hash.cpp
+++ Source/Core/Common/Hash.cpp
@@ -6,12 +6,11 @@
 #include "Common/Common.h"
 #include "Common/CommonFuncs.h"
 #include "Common/Hash.h"
+#include "xxhash.h"
 #if _M_SSE >= 0x402
 #include "Common/CPUDetect.h"
 #include <nmmintrin.h>
 #endif
-
-static u64 (*ptrHashFunction)(const u8 *src, int len, u32 samples) = &GetMurmurHash3;
 
 // uint32_t
 // WARNING - may read one more byte!
@@ -103,130 +102,6 @@ u32 HashEctor(const u8* ptr, int length)
 
 #if _ARCH_64
 
-//-----------------------------------------------------------------------------
-// Block read - if your platform needs to do endian-swapping or can only
-// handle aligned reads, do the conversion here
-
-inline u64 getblock(const u64 * p, int i)
-{
-	return p[i];
-}
-
-//----------
-// Block mix - combine the key bits with the hash bits and scramble everything
-
-inline void bmix64(u64 & h1, u64 & h2, u64 & k1, u64 & k2, u64 & c1, u64 & c2)
-{
-	k1 *= c1;
-	k1  = _rotl64(k1,23);
-	k1 *= c2;
-	h1 ^= k1;
-	h1 += h2;
-
-	h2 = _rotl64(h2,41);
-
-	k2 *= c2;
-	k2  = _rotl64(k2,23);
-	k2 *= c1;
-	h2 ^= k2;
-	h2 += h1;
-
-	h1 = h1*3+0x52dce729;
-	h2 = h2*3+0x38495ab5;
-
-	c1 = c1*5+0x7b7d159c;
-	c2 = c2*5+0x6bce6396;
-}
-
-//----------
-// Finalization mix - avalanches all bits to within 0.05% bias
-
-inline u64 fmix64(u64 k)
-{
-	k ^= k >> 33;
-	k *= 0xff51afd7ed558ccd;
-	k ^= k >> 33;
-	k *= 0xc4ceb9fe1a85ec53;
-	k ^= k >> 33;
-
-	return k;
-}
-
-u64 GetMurmurHash3(const u8 *src, int len, u32 samples)
-{
-	const u8 * data = (const u8*)src;
-	const int nblocks = len / 16;
-	u32 Step = (len / 8);
-	if (samples == 0) samples = std::max(Step, 1u);
-	Step = Step / samples;
-	if (Step < 1) Step = 1;
-
-	u64 h1 = 0x9368e53c2f6af274;
-	u64 h2 = 0x586dcd208f7cd3fd;
-
-	u64 c1 = 0x87c37b91114253d5;
-	u64 c2 = 0x4cf5ad432745937f;
-
-
-	//----------
-	// body
-
-	const u64 * blocks = (const u64 *)(data);
-
-	for (int i = 0; i < nblocks; i+=Step)
-	{
-		u64 k1 = getblock(blocks,i*2+0);
-		u64 k2 = getblock(blocks,i*2+1);
-
-		bmix64(h1,h2,k1,k2,c1,c2);
-	}
-
-	//----------
-	// tail
-
-	const u8 * tail = (const u8*)(data + nblocks*16);
-
-	u64 k1 = 0;
-	u64 k2 = 0;
-
-	switch (len & 15)
-	{
-	case 15: k2 ^= u64(tail[14]) << 48;
-	case 14: k2 ^= u64(tail[13]) << 40;
-	case 13: k2 ^= u64(tail[12]) << 32;
-	case 12: k2 ^= u64(tail[11]) << 24;
-	case 11: k2 ^= u64(tail[10]) << 16;
-	case 10: k2 ^= u64(tail[ 9]) << 8;
-	case  9: k2 ^= u64(tail[ 8]) << 0;
-
-	case  8: k1 ^= u64(tail[ 7]) << 56;
-	case  7: k1 ^= u64(tail[ 6]) << 48;
-	case  6: k1 ^= u64(tail[ 5]) << 40;
-	case  5: k1 ^= u64(tail[ 4]) << 32;
-	case  4: k1 ^= u64(tail[ 3]) << 24;
-	case  3: k1 ^= u64(tail[ 2]) << 16;
-	case  2: k1 ^= u64(tail[ 1]) << 8;
-	case  1: k1 ^= u64(tail[ 0]) << 0;
-			bmix64(h1,h2,k1,k2,c1,c2);
-	};
-
-	//----------
-	// finalization
-
-	h2 ^= len;
-
-	h1 += h2;
-	h2 += h1;
-
-	h1 = fmix64(h1);
-	h2 = fmix64(h2);
-
-	h1 += h2;
-
-	return h1;
-}
-
-
 // CRC32 hash using the SSE4.2 instruction
 u64 GetCRC32(const u8 *src, int len, u32 samples)
 {
@@ -262,6 +137,10 @@ u64 GetCRC32(const u8 *src, int len, u32 samples)
 #endif
 }
 
+u64 GetXXHash(const u8 *src, int len, u32 samples)
+{
+	return XXH64(src, len, samples);
+}
 
 /*
  * NOTE: This hash function is used for custom texture loading/dumping, so
@@ -337,123 +216,9 @@ u64 GetCRC32(const u8 *src, int len, u32 samples)
 #endif
 }
 
-//-----------------------------------------------------------------------------
-// Block read - if your platform needs to do endian-swapping or can only
-// handle aligned reads, do the conversion here
-
-inline u32 getblock(const u32 * p, int i)
+u64 GetXXHash(const u8 *src, int len, u32 samples)
 {
-	return p[i];
-}
-
-//----------
-// Finalization mix - force all bits of a hash block to avalanche
-
-// avalanches all bits to within 0.25% bias
-
-inline u32 fmix32(u32 h)
-{
-	h ^= h >> 16;
-	h *= 0x85ebca6b;
-	h ^= h >> 13;
-	h *= 0xc2b2ae35;
-	h ^= h >> 16;
-
-	return h;
-}
-
-inline void bmix32(u32 & h1, u32 & h2, u32 & k1, u32 & k2, u32 & c1, u32 & c2)
-{
-	k1 *= c1;
-	k1  = _rotl(k1,11);
-	k1 *= c2;
-	h1 ^= k1;
-	h1 += h2;
-
-	h2 = _rotl(h2,17);
-
-	k2 *= c2;
-	k2  = _rotl(k2,11);
-	k2 *= c1;
-	h2 ^= k2;
-	h2 += h1;
-
-	h1 = h1*3+0x52dce729;
-	h2 = h2*3+0x38495ab5;
-
-	c1 = c1*5+0x7b7d159c;
-	c2 = c2*5+0x6bce6396;
-}
-
-//----------
-
-u64 GetMurmurHash3(const u8* src, int len, u32 samples)
-{
-	const u8 * data = (const u8*)src;
-	u32 out[2];
-	const int nblocks = len / 8;
-	u32 Step = (len / 4);
-	if (samples == 0) samples = std::max(Step, 1u);
-	Step = Step / samples;
-	if (Step < 1) Step = 1;
-
-	u32 h1 = 0x8de1c3ac;
-	u32 h2 = 0xbab98226;
-
-	u32 c1 = 0x95543787;
-	u32 c2 = 0x2ad7eb25;
-
-	//----------
-	// body
-
-	const u32 * blocks = (const u32 *)(data + nblocks*8);
-
-	for (int i = -nblocks; i < 0; i+=Step)
-	{
-		u32 k1 = getblock(blocks,i*2+0);
-		u32 k2 = getblock(blocks,i*2+1);
-
-		bmix32(h1,h2,k1,k2,c1,c2);
-	}
-
-	//----------
-	// tail
-
-	const u8 * tail = (const u8*)(data + nblocks*8);
-
-	u32 k1 = 0;
-	u32 k2 = 0;
-
-	switch (len & 7)
-	{
-	case 7: k2 ^= tail[6] << 16;
-	case 6: k2 ^= tail[5] << 8;
-	case 5: k2 ^= tail[4] << 0;
-	case 4: k1 ^= tail[3] << 24;
-	case 3: k1 ^= tail[2] << 16;
-	case 2: k1 ^= tail[1] << 8;
-	case 1: k1 ^= tail[0] << 0;
-	        bmix32(h1,h2,k1,k2,c1,c2);
-	};
-
-	//----------
-	// finalization
-
-	h2 ^= len;
-
-	h1 += h2;
-	h2 += h1;
-
-	h1 = fmix32(h1);
-	h2 = fmix32(h2);
-
-	h1 += h2;
-	h2 += h1;
-
-	out[0] = h1;
-	out[1] = h2;
-
-	return *((u64 *)&out);
+	return (u64)XXH32(src, len, samples);
 }
 
 /*
@@ -505,6 +270,8 @@ u64 GetHashHiresTexture(const u8 *src, int len, u32 samples)
 }
 #endif
 
+static u64(*ptrHashFunction)(const u8 *src, int len, u32 samples) = &GetXXHash;
+
 u64 GetHash64(const u8 *src, int len, u32 samples)
 {
 	return ptrHashFunction(src, len, samples);
@@ -514,7 +281,7 @@ u64 GetHash64(const u8 *src, int len, u32 samples)
 void SetHash64Function()
 {
 #if _M_SSE >= 0x402
-	// sse4.2 crc32 version used only on intel, in amd is slower than murmurhash3
+	// sse4.2 crc32 version used only on intel, in amd is slower than xxhash
 	if (cpu_info.vendor == VENDOR_INTEL && cpu_info.bSSE4_2) 
 	{
 		ptrHashFunction = &GetCRC32;
@@ -522,7 +289,7 @@ void SetHash64Function()
 	else
 #endif
 	{
-		ptrHashFunction = &GetMurmurHash3;
+		ptrHashFunction = &GetXXHash;
 	}
 }
 
