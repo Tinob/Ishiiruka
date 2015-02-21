2709c0c57c2324800288b240df95851f6958e94a
Rodolfo Bogado <rodolfoosvaldobogado@gmail.com>
2015-02-21 14:53:47 -0300

    some fixes to efb depalettizer, implemented compute shader support for hard that supports it.

--- Source/Core/VideoBackends/DX11/CSTextureDecoder.cpp
+++ Source/Core/VideoBackends/DX11/CSTextureDecoder.cpp
@@ -22,10 +22,8 @@ namespace DX11
 
 static const char DECODER_CS[] = R"HLSL(
 //
-
 Buffer<uint> rawData_ : register(t0);
 Buffer<uint> lutData_ : register(t1);
-Texture2D<float4> sourceData_ : register(t2);
 
 RWTexture2D<uint4> dstTexture_ : register(u0);
 
@@ -33,7 +31,6 @@ uint2 dims_ : register(b0);
 
 uint ReadByte( uint addr ) {
 	uint r = rawData_[addr>>2];
-
 	return (r >> (8*(addr&0x3)))&0xff;
 }
 
@@ -45,13 +42,11 @@ uint ReadTwoBytes( uint addr ) {
 
 uint ReadFourBytes( uint addr ) {
 	uint r = rawData_[addr>>2];
-	
 	return ((r&0xff)<<24) | ((r&0xff00)<<8) | ((r&0xff000000)>>24) | ((r&0xff0000)>>8);
 }
 
 uint ReadFourBytesNoSwap( uint addr ) {
 	uint r = rawData_[addr>>2];
-	
 	return r;
 }
 
@@ -82,7 +77,7 @@ uint4 Read565( uint v ) {
 	uint b = (v&0x1F)<<3; b |= b >> 5;
 	uint g = (v&0x7E0)>>3; g |= g >> 6;
 	uint r = (v&0xF800)>>8; r |= r >> 5;
-  return uint4(r,g,b,255);
+	return uint4(r,g,b,255);
 }
 
 
@@ -95,7 +90,7 @@ uint4 ReadLutIA8( uint idx ) {
 uint4 ReadLut565( uint idx ) {
 	uint v = lutData_[idx];
 	v = ((v&0xff)<<8)| ((v&0xff00)>>8);
-  return Read565(v);
+	return Read565(v);
 }
 
 uint4 ReadLut5A3( uint idx ) {
@@ -107,27 +102,27 @@ uint4 ReadLut5A3( uint idx ) {
 uint Decode2Nibbles( uint2 st ) {
 	uint pitch = ((dims_.x+7)/8)*32;
 	uint tile = (st.y/8) * pitch + 32 * (st.x/8);
-  uint offs = (st.x&0x7) + (st.y&0x7)*4;
+	uint offs = (st.x&0x7) + (st.y&0x7)*4;
 	return ReadByte( tile + offs );
 }
 
 uint Decode1Byte( uint2 st ) {
 	uint pitch = ((dims_.x+7)/8)*32;
 	uint tile = (st.y/4) * pitch + 32 * (st.x/8);
-  uint offs = (st.x&0x7) + (st.y&0x3)*8;
+	uint offs = (st.x&0x7) + (st.y&0x3)*8;
 	return ReadByte( tile + offs );
 }
 
 uint Decode2Bytes( uint2 st ) {
 	uint pitch = ((dims_.x+3)/4)*32;
 	uint tile = (st.y/4) * pitch + 32 * (st.x/4);
-  uint offs = (st.x&0x3)*2 + (st.y&0x3)*8;
+	uint offs = (st.x&0x3)*2 + (st.y&0x3)*8;
 	return ReadTwoBytes( tile + offs );
 }
 
 uint4 DecodeI8( uint2 st ) {
 	uint v = Decode1Byte( st);
-  return v.xxxx;
+	return v.xxxx;
 }
 
 uint4 DecodeC8( uint2 st ) {
@@ -154,9 +149,9 @@ uint4 DecodeI4( uint2 st ) {
 
 	uint v = ReadByte( offset );
 	uint i = (blkOff&1) ? (v & 0x0F): ((v & 0xF0)>>4);
-  i <<= 4;
+	i <<= 4;
 	i |= (i>>4);
-  return i.xxxx;
+	return i.xxxx;
 }
 uint4 DecodeC4( uint2 st ) {
 	uint sBlk = st.x >> 3;
@@ -186,7 +181,7 @@ uint4 DecodeIA8( uint2 st ) {
 	uint v = Decode2Bytes( st );
 	uint i = (v&0xFF);
 	uint a = (v&0xFF00)>>8;
-  return uint4(i.xxx,a);
+	return uint4(i.xxx,a);
 }
 
 uint4 NotImplemented( uint2 st ) {
@@ -194,10 +189,10 @@ uint4 NotImplemented( uint2 st ) {
 }
 
 uint4 DecodeIA4( uint2 st ) {
-  uint v = Decode1Byte( st );
+	uint v = Decode1Byte( st );
 	uint i = (v&0x0F)<<4; i |= (i>>4);
 	uint a = (v&0xF0); a |= (a>>4);
-  return uint4(i.xxx,a);
+	return uint4(i.xxx,a);
 }
 
 
@@ -211,14 +206,14 @@ uint4 DecodeCmpr( uint2 st ) {
 	uint pitch = ((dims_.x+7)/8)*32;
 	uint tile = (st.y/8) * pitch + 32 * (st.x/8);
 
-  uint2 pix = st & 0x7;
+	uint2 pix = st & 0x7;
 	uint offs = 8 * ( pix.x/4 ) + 16 * (pix.y/4);
-  uint col0 = ReadTwoBytes( tile + offs + 0 );
-  uint col1 = ReadTwoBytes( tile + offs + 2 );
-  uint lut = ReadFourBytes( tile + offs + 4 );
+	uint col0 = ReadTwoBytes( tile + offs + 0 );
+	uint col1 = ReadTwoBytes( tile + offs + 2 );
+	uint lut = ReadFourBytes( tile + offs + 4 );
 
-  uint2 px = st & 0x3;
-  uint idx = ( lut >> ( 32 - (px.x*2+2) - px.y*8 ) ) & 3;
+	uint2 px = st & 0x3;
+	uint idx = ( lut >> ( 32 - (px.x*2+2) - px.y*8 ) ) & 3;
 
 	uint3 c0,c1;
 	c0 = Read565(col0).rgb;
@@ -246,7 +241,7 @@ uint4 DecodeCmpr( uint2 st ) {
 uint4 DecodeRGBA8( uint2 st ) {
 	uint pitch = ((dims_.x+3)/4)*64;
 	uint tile = (st.y/4) * pitch + 64 * (st.x/4);
-  uint offs = (st.x&0x3)*2 + (st.y&0x3)*8;
+	uint offs = (st.x&0x3)*2 + (st.y&0x3)*8;
 
 	uint ar = ReadTwoBytes( tile + offs );
 	uint gb = ReadTwoBytes( tile + offs + 32);
@@ -258,7 +253,7 @@ uint4 DecodeRGBA8FromTMEM( uint2 st ) {
 	uint bgoffs = 2 * ((dims_.x+3)&~3) * ((dims_.y+3)&~3);
 	uint pitch = ((dims_.x+3)/4)*32;
 	uint tile = (st.y/4) * pitch + 32 * (st.x/4);
-  uint offs = (st.x&0x3)*2 + (st.y&0x3)*8;
+	uint offs = (st.x&0x3)*2 + (st.y&0x3)*8;
 
 	uint ar = ReadTwoBytes( tile + offs );
 	uint gb = ReadTwoBytes( tile + offs + bgoffs);
@@ -266,18 +261,9 @@ uint4 DecodeRGBA8FromTMEM( uint2 st ) {
 	return uint4( (ar&0x00ff)>>0, (gb&0xff00)>>8, (gb&0x00ff)>>0, (ar&0xff00)>>8);
 }
 
-uint4 DepalettizeC4( uint2 st ) {
-	uint idx = uint(round(sourceData_.Load(int3(st.xy, 0)).r * 15.0));
-	return LUT_FUNC(idx);
-}
-
-uint4 DepalettizeC8( uint2 st ) {
-	uint idx = uint(round(sourceData_.Load(int3(st.xy, 0)).r * 255.0));
-	return LUT_FUNC(idx);	
-}
-
 [numthreads(8,8,1)]
-void main(in uint3 groupIdx : SV_GroupID, in uint3 subgroup : SV_GroupThreadID) {
+void main(in uint3 groupIdx : SV_GroupID, in uint3 subgroup : SV_GroupThreadID)
+{
 	uint2 st = groupIdx.xy * 8 + subgroup.xy;
 	if ( all( st < dims_) ) 
 	{
@@ -288,10 +274,11 @@ void main(in uint3 groupIdx : SV_GroupID, in uint3 subgroup : SV_GroupThreadID) 
 )HLSL";
 
 static const char DEPALETTIZE_SHADER[] = R"HLSL(
-sampler samp0 : register(s0);
 Texture2D Tex0 : register(t0);
 Buffer<uint> Tex1 : register(t1);
-
+#if USE_COMPUTE
+RWTexture2D<float4> dstTexture_ : register(u0);
+#endif
 uint Convert3To8(uint v)
 {
 	// Swizzle bits: 00000123 -> 12312312
@@ -353,20 +340,38 @@ float4 ReadLutIA8(uint val)
 	return float4(i, i, i, a) / 255;
 }
 
+#if USE_COMPUTE
+[numthreads(8,8,1)]
+void main(
+	in uint3 groupIdx : SV_GroupID, 
+	in uint3 subgroup : SV_GroupThreadID) 
+{
+	uint2 pos = groupIdx.xy * 8 + subgroup.xy;
+	uint2 dims_;
+	dstTexture_.GetDimensions(dims_.x,dims_.y);	
+	if ( all( pos < dims_) ) 	
+#else
 void main(
 	out float4 ocol0 : SV_Target,
 	in float4 pos : SV_Position,
 	in float3 uv0 : TEXCOORD0)
 {
-#if NUM_COLORS == 16
-	float Multiply = 15.0;
-#else
-	float Multiply = 255.0;
 #endif
-	uint src = round(Tex0.Sample(samp0,uv0) * Multiply).r;
-	src = Tex1.Load(src);
-	src = ((src << 8) & 0xFF00) | (src >> 8);
-	ocol0 = LUT_FUNC(src);
+	{
+#if NUM_COLORS == 16
+		float Multiply = 15.0;
+#else
+		float Multiply = 255.0;
+#endif
+		uint src = round(Tex0.Load(int3(pos.xy, 0)) * Multiply).r;
+		src = Tex1.Load(src);
+		src = ((src << 8) & 0xFF00) | (src >> 8);
+#if USE_COMPUTE
+		dstTexture_[pos.xy] = LUT_FUNC( src ); 
+#else
+		ocol0 = LUT_FUNC(src);
+#endif
+	}
 }
 )HLSL";
 
@@ -474,9 +479,9 @@ char const* DecFunc[] = {
 "DecodeRGBA8FromTMEM",//
 };
 
-char const* DepalettizeFuncs[] = {
-"DepalettizeC4",
-"DepalettizeC8"
+char const* DepalettizeColors[] = {
+"16",
+"256"
 };
 
 char const* LutFunc[] = {
@@ -588,14 +593,15 @@ bool CSTextureDecoder::SetDepalettizeShader(BaseType srcFmt, u32 lutFmt)
 
 	D3D_SHADER_MACRO macros[] =
 	{
-		{ "DECODER_FUNC", DepalettizeFuncs[u32(srcFmt) & 1] },
+		{ "USE_COMPUTE", "1" },
+		{ "NUM_COLORS", DepalettizeColors[u32(srcFmt) & 1] },
 		{ "LUT_FUNC", LutFunc[lutFmt & 3] },
 		{ nullptr, nullptr }
 	};
 
 	D3DBlob bytecode = nullptr;
-	if (!D3D::CompileShader(D3D::ShaderType::Compute, DECODER_CS, bytecode, macros)) {
-		WARN_LOG(VIDEO, "noooo");
+	if (!D3D::CompileShader(D3D::ShaderType::Compute, DEPALETTIZE_SHADER, bytecode, macros)) {
+		WARN_LOG(VIDEO, "Unable to compile Depalettize compute shader");
 	}
 
 	m_shaderCache.Append(key, bytecode.Data(), u32(bytecode.Size()));
@@ -763,7 +769,7 @@ bool CSTextureDecoder::DecodeRGBAFromTMEM( u8 const * ar_src, u8 const * bg_src,
 
 bool CSTextureDecoder::Depalettize(D3DTexture2D& dstTexture, D3DTexture2D& srcTexture, BaseType baseType, u32 width, u32 height)
 {
-	if (true/*D3D::GetFeatureLevel() < D3D_FEATURE_LEVEL_11_0*/) // Disable compute shader support is not working rigth now
+	if (D3D::GetFeatureLevel() < D3D_FEATURE_LEVEL_11_0)
 	{
 		ID3D11PixelShader* shader = GetDepalettizerPShader(baseType, m_lutFmt);
 		if (!shader)
@@ -771,15 +777,16 @@ bool CSTextureDecoder::Depalettize(D3DTexture2D& dstTexture, D3DTexture2D& srcTe
 			return false;
 		}
 
-		g_renderer->ResetAPIState();
-		D3D::stateman->Apply();
+		g_renderer->ResetAPIState();		
 
 		D3D11_VIEWPORT vp = CD3D11_VIEWPORT(0.f, 0.f, FLOAT(width), FLOAT(height));
 		D3D::context->RSSetViewports(1, &vp);
 
-		D3D::context->OMSetRenderTargets(1, &dstTexture.GetRTV(), NULL);
-		D3D::context->PSSetShaderResources(1, 1, D3D::ToAddr(m_lutSrv));
+		D3D::stateman->SetTexture(1, m_lutSrv.get());
 		D3D11_RECT rsource = { 0, 0, width, height };
+
+		D3D::context->OMSetRenderTargets(1, &dstTexture.GetRTV(), nullptr);
+		
 		D3D::drawShadedTexQuad(
 			srcTexture.GetSRV(),
 			&rsource,
@@ -788,15 +795,12 @@ bool CSTextureDecoder::Depalettize(D3DTexture2D& dstTexture, D3DTexture2D& srcTe
 			VertexShaderCache::GetSimpleVertexShader(),
 			VertexShaderCache::GetSimpleInputLayout()
 			);
-
-		ID3D11ShaderResourceView* nullDummy = NULL;
-		D3D::context->PSSetShaderResources(0, 1, &nullDummy);
-		D3D::context->PSSetShaderResources(1, 1, &nullDummy);
-
-		g_renderer->RestoreAPIState();
+		
+		D3D::stateman->SetTexture(1, nullptr);
 		D3D::context->OMSetRenderTargets(1,
 			&FramebufferManager::GetEFBColorTexture()->GetRTV(),
 			FramebufferManager::GetEFBDepthTexture()->GetDSV());
+		g_renderer->RestoreAPIState();
 	}
 	else
 	{
@@ -804,24 +808,16 @@ bool CSTextureDecoder::Depalettize(D3DTexture2D& dstTexture, D3DTexture2D& srcTe
 		{
 			return false;
 		}
-		ID3D11UnorderedAccessView* uav = dstTexture.GetUAV();
-		D3D::context->CSSetUnorderedAccessViews(0, 1, &uav, nullptr);
+		D3D::context->CSSetUnorderedAccessViews(0, 1, &dstTexture.GetUAV(), nullptr);
 
-		ID3D11ShaderResourceView* srvs[] = { nullptr, m_lutSrv.get(), srcTexture.GetSRV() };
-		D3D::context->CSSetShaderResources(0, 3, srvs);
-
-		D3D11_MAPPED_SUBRESOURCE map;
-		D3D::context->Map(m_params.get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &map);
-		((u32*)map.pData)[0] = width;
-		((u32*)map.pData)[1] = height;
-		D3D::context->Unmap(m_params.get(), 0);
-		D3D::context->CSSetConstantBuffers(0, 1, D3D::ToAddr(m_params));
+		ID3D11ShaderResourceView* srvs[] = { srcTexture.GetSRV(), m_lutSrv.get() };
+		D3D::context->CSSetShaderResources(0, 2, srvs);		
 
 		D3D::context->Dispatch((width + 7) / 8, (height + 7) / 8, 1);
-		uav = nullptr;
+		ID3D11UnorderedAccessView* uav = nullptr;
 		D3D::context->CSSetUnorderedAccessViews(0, 1, &uav, nullptr);
 		ID3D11ShaderResourceView* srv = nullptr;
-		D3D::context->CSSetShaderResources(2, 1, &srv);
+		D3D::context->CSSetShaderResources(0, 1, &srv);
 	}
 	return true;
 }
