c0fcb605d01b0c9349bd01ec88f10dc8e3899cd9
Rodolfo Bogado <rodolfoosvaldobogado@gmail.com>
2015-05-01 17:41:59 -0300

    fix for a bug in geometrishadergen

--- Source/Core/Common/make_scmrev.h.js
+++ Source/Core/Common/make_scmrev.h.js
@@ -1,40 +1,38 @@
-var wshShell		= new ActiveXObject("WScript.Shell")
-var oFS				= new ActiveXObject("Scripting.FileSystemObject");
+var wshShell = new ActiveXObject("WScript.Shell")
+var oFS = new ActiveXObject("Scripting.FileSystemObject");
 
-var outfile			= "./scmrev.h";
-var cmd_revision    = " rev-parse HEAD";
-var cmd_count       = " rev-list --count HEAD ^e1656af8191700f32c18b06d18b9a099d281b95b";
-var cmd_describe	= " describe --always --long --dirty";
-var cmd_branch		= " rev-parse --abbrev-ref HEAD";
+var outfile = "./scmrev.h";
+var cmd_revision = " rev-parse HEAD";
+var cmd_count = " rev-list --count HEAD ^e1656af8191700f32c18b06d18b9a099d281b95b";
+var cmd_describe = " describe --always --long --dirty";
+var cmd_branch = " rev-parse --abbrev-ref HEAD";
 var cmd_lastmodified = " log --pretty=oneline  -n 1 -- ";
 var cache_modifier_files = new Array(
-    "../VideoCommon/PixelShaderGen.cpp",
-    "../VideoCommon/VertexShaderGen.cpp",
-    "../VideoCommon/LightingShaderGen.h",
-    "../VideoCommon/PixelShaderGen.h",
-    "../VideoCommon/ShaderGenCommon.h",
-    "../VideoCommon/VertexShaderGen.h",
-    "../VideoCommon/TextureConversionShader.cpp",
-    "../VideoCommon/TextureConversionShader.h");
+	"../VideoCommon/PixelShaderGen.cpp",
+	"../VideoCommon/VertexShaderGen.cpp",
+	"../VideoCommon/LightingShaderGen.h",
+	"../VideoCommon/PixelShaderGen.h",
+	"../VideoCommon/ShaderGenCommon.h",
+	"../VideoCommon/VertexShaderGen.h",
+	"../VideoCommon/GeometryShaderGen.cpp",
+	"../VideoCommon/GeometryShaderGen.h");
 function GetGitExe()
 {
-    try {
-        gitexe = wshShell.RegRead("HKCU\\Software\\GitExtensions\\gitcommand");
-        wshShell.Exec(gitexe);
-        return gitexe;
-    }
-    catch (e)
-    { }
+	try {
+		gitexe = wshShell.RegRead("HKCU\\Software\\GitExtensions\\gitcommand");
+		wshShell.Exec(gitexe);
+		return gitexe;
+	}
+	catch (e)
+	{ }
 
-	for (var gitexe in {"git.cmd":1, "git":1, "git.bat":1})
-	{
-		try
-		{
+	for (var gitexe in { "git.cmd": 1, "git": 1, "git.bat": 1 }) {
+		try {
 			wshShell.Exec(gitexe);
 			return gitexe;
 		}
 		catch (e)
-		{}
+		{ }
 	}
 
 	WScript.Echo("Cannot find git or git.cmd, check your PATH:\n" +
@@ -44,12 +42,10 @@ function GetGitExe()
 
 function GetFirstStdOutLine(cmd)
 {
-	try
-	{
+	try {
 		return wshShell.Exec(cmd).StdOut.ReadLine();
 	}
-	catch (e)
-	{
+	catch (e) {
 		// catch "the system cannot find the file specified" error
 		WScript.Echo("Failed to exec " + cmd + " this should never happen");
 		WScript.Quit(1);
@@ -58,47 +54,46 @@ function GetFirstStdOutLine(cmd)
 
 function GetFileContents(f)
 {
-	try
-	{
+	try {
 		return oFS.OpenTextFile(f).ReadAll();
 	}
-	catch (e)
-	{
+	catch (e) {
 		// file doesn't exist
 		return "";
 	}
 }
 
-function GetCacheVersion(gitexe) {
-    var cacheversion = "";    
-    try {
-        var fragmentlen = 40 / cache_modifier_files.length;
-        if (fragmentlen < 1) {
-            fragmentlen = 1;
-        }
-        while ((fragmentlen * cache_modifier_files.length) < 40) {
-            fragmentlen++;
-        }        
-        for (var i = 0; i < cache_modifier_files.length; i++) {
-            var cmd = gitexe + cmd_lastmodified + cache_modifier_files[i];
-            var fileversion = wshShell.Exec(cmd).StdOut.ReadLine();
-            cacheversion += fileversion.substr(0, fragmentlen);
-        }
-    }
-    catch (e) {
-        cacheversion = e.toString();
-    }
-    return cacheversion.substr(0, 40);
+function GetCacheVersion(gitexe)
+{
+	var cacheversion = "";
+	try {
+		var fragmentlen = 40 / cache_modifier_files.length;
+		if (fragmentlen < 1) {
+			fragmentlen = 1;
+		}
+		while ((fragmentlen * cache_modifier_files.length) < 40) {
+			fragmentlen++;
+		}
+		for (var i = 0; i < cache_modifier_files.length; i++) {
+			var cmd = gitexe + cmd_lastmodified + cache_modifier_files[i];
+			var fileversion = wshShell.Exec(cmd).StdOut.ReadLine();
+			cacheversion += fileversion.substr(0, fragmentlen);
+		}
+	}
+	catch (e) {
+		cacheversion = e.toString();
+	}
+	return cacheversion.substr(0, 40);
 }
 
 // get info from git
 var gitexe = GetGitExe();
 var revision = GetFirstStdOutLine(gitexe + cmd_revision);
 var revcount = GetFirstStdOutLine(gitexe + cmd_count);
-var describe	 = GetFirstStdOutLine(gitexe + cmd_describe);
-var branch       = GetFirstStdOutLine(gitexe + cmd_branch);
+var describe = GetFirstStdOutLine(gitexe + cmd_describe);
+var branch = GetFirstStdOutLine(gitexe + cmd_branch);
 var cacheversion = GetCacheVersion(gitexe);
-var isMaster    = +("master" == branch);
+var isMaster = +("master" == branch);
 
 // remove hash (and trailing "-0" if needed) from description
 describe = describe.replace(/(-0)?-[^-]+(-dirty)?$/, '$2');
--- Source/Core/VideoCommon/GeometryShaderGen.cpp
+++ Source/Core/VideoCommon/GeometryShaderGen.cpp
@@ -249,7 +249,7 @@ static inline void GenerateGeometryShader(T& out, u32 primitive_type, const XFMe
 		out.Write("\tif (" I_TEXOFFSET"[2] != 0) {\n");
 		out.Write("\tfloat texOffset = 1.0 / float(" I_TEXOFFSET"[2]);\n");
 
-		for (unsigned int i = 0; i < bpmem.genMode.numtexgens; ++i)
+		for (unsigned int i = 0; i < bpm.genMode.numtexgens; ++i)
 		{
 			out.Write("\tif (((" I_TEXOFFSET"[0] >> %d) & 0x1) != 0)\n", i);
 			out.Write("\t\tr.tex%d.x += texOffset;\n", i);
@@ -274,7 +274,7 @@ static inline void GenerateGeometryShader(T& out, u32 primitive_type, const XFMe
 		out.Write("\tif (" I_TEXOFFSET"[3] != 0) {\n");
 		out.Write("\tfloat2 texOffset = float2(1.0 / float(" I_TEXOFFSET"[3]), 1.0 / float(" I_TEXOFFSET"[3]));\n");
 
-		for (unsigned int i = 0; i < bpmem.genMode.numtexgens; ++i)
+		for (unsigned int i = 0; i < bpm.genMode.numtexgens; ++i)
 		{
 			out.Write("\tif (((" I_TEXOFFSET"[1] >> %d) & 0x1) != 0) {\n", i);
 			out.Write("\t\tll.tex%d.xy += float2(0,1) * texOffset;\n", i);
