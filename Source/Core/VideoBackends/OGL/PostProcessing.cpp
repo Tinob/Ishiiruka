36a7b07d1d3d6297b0a123be8578a1422141faf9
Vincent Imbimbo <vmi6@cornell.edu>
2017-06-04 13:50:15 -0400

    Working mac version

--- Source/Core/VideoBackends/OGL/PostProcessing.cpp
+++ Source/Core/VideoBackends/OGL/PostProcessing.cpp
@@ -319,7 +319,7 @@ void OGLPostProcessingShader::Draw(PostProcessor* p,
 		for (size_t i = 0; i < pass.inputs.size(); i++)
 		{
 			const InputBinding& input = pass.inputs[i];
-			glActiveTexture(GL_TEXTURE0 + FIRST_INPUT_TEXTURE_UNIT + input.texture_unit);
+			glActiveTexture(GL_TEXTURE0 + input.texture_unit);
 
 			switch (input.type)
 			{
--- Source/Core/VideoBackends/OGL/RasterFont.cpp
+++ Source/Core/VideoBackends/OGL/RasterFont.cpp
@@ -140,6 +140,8 @@ RasterFont::RasterFont()
 {
 	// generate the texture
 	glActiveTexture(GL_TEXTURE8);
+    glGenTextures(1,&texture);
+    glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
 	std::vector<u32> texture_data(CHARACTER_WIDTH * CHARACTER_COUNT * CHARACTER_HEIGHT);
 	for (int y = 0; y < CHARACTER_HEIGHT; y++)
 	{
@@ -258,7 +260,7 @@ void RasterFont::printMultilineText(const std::string& text, double start_x, dou
 	{
 		return;
 	}
-
+    glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, usage * sizeof(GLfloat), vertices.data(), GL_STREAM_DRAW);
