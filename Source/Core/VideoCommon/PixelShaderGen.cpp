9a9756da593fcd5ade0dcf06ebc1ea6571c27ef4
Rodolfo Bogado <rodolfoosvaldobogado@gmail.com>
2019-09-02 23:53:56 -0300

    fix some issues with dithering

--- Source/Core/VideoCommon/PixelShaderGen.cpp
+++ Source/Core/VideoCommon/PixelShaderGen.cpp
@@ -1638,12 +1638,13 @@ inline void GeneratePixelShader(ShaderCode& out, const pixel_shader_uid_data& ui
   if (uid_data.dither)
   {
     out.Write("wu GetDitherValue(wu2 ditherindex)\n{\n");
+    out.Write("\twu4 bayer[4] = {wu4(-2,0,1,-1),wu4(-1,1,-2,0),wu4(1,3,-1,-2),wu4(0,-1,2,1)};\n");
+    out.Write("\twu result = bayer[ditherindex.y][ditherindex.x];\n");
     if (uid_data.rgba6_format)
-      out.Write("\twu4 bayer[4] = {wu4(-8,0,-6,2),wu4(4,-4,6,-2),wu4(-5,3,-7,1),wu4(7,-1,5,-3)};\n");
-    else
-      out.Write("\tint4 bayer[4] = {wu4(-2,0,1,-1),wu4(-1,1,-2,0),wu4(1,3,-1,-2),wu4(0,-1,2,1)};\n");
-    out.Write("\treturn bayer[ditherindex.y][ditherindex.x];\n");
-    out.Write("}\n");
+    {
+      out.Write("\tresult *= 3;\n");
+    }
+    out.Write("\treturn result;\n}\n");
   }
   if (ApiType == API_OPENGL || ApiType == API_VULKAN)
   {
@@ -1845,17 +1846,6 @@ inline void GeneratePixelShader(ShaderCode& out, const pixel_shader_uid_data& ui
     out.Write("}\n");
     return;
   }
-  if (uid_data.dither)
-  {
-	  if (ApiType & API_D3D9)
-	  {
-		  out.Write("\tfloat2 ditherindex = round(rawpos.xy) %% 4;\n");
-	  }
-	  else
-	  {
-		  out.Write("\tint2 ditherindex = int2(rawpos.xy) & 3;\n");
-	  }
-  }
   out.Write("wu4 c0 = " I_COLORS "[1], c1 = " I_COLORS "[2], c2 = " I_COLORS "[3], prev = " I_COLORS "[0];\n"
     "wu4 tex_ta[%i], tex_t = wu4(0,0,0,0), ras_t = wu4(0,0,0,0), konst_t = wu4(0,0,0,0);\n"
     "wu3 c16 = wu3(1,256,0), c24 = wu3(1,256,256*256);\n"
@@ -1916,6 +1906,18 @@ inline void GeneratePixelShader(ShaderCode& out, const pixel_shader_uid_data& ui
     {
       out.Write("\tuv%d.xy = uv%d.xy / ((uv%d.z == 0.0) ? 2.0 : uv%d.z);\n", i, i, i, i);
       out.Write("uv%d.xy = trunc(uv%d.xy * " I_TEXDIMS"[%d].zw);\n", i, i, i);
+    }
+  }
+
+  if (uid_data.dither)
+  {
+    if (ApiType & API_D3D9)
+    {
+      out.Write("\tfloat2 ditherindex = round(rawpos.xy) %% 4;\n");
+    }
+    else
+    {
+      out.Write("\tint2 ditherindex = int2(rawpos.xy) & 3;\n");
     }
   }
 
